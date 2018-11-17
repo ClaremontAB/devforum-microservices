e7f78bbc65bd91630f0cd9de4351bfb2650488bf
Andreas Smidelöv <Andreas.Smidelov@claremont.se>
2018-11-17 14:52:09 +0100

    Initial .NET commit

---
+++ net-clarenews/Clarenews/Controllers/ClarenewsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Clarenews.Controllers
+{
+    [Route("api/[controller]")]
+    public class ClarenewsController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Artikel> Get()
+        {
+            var client = new HttpClient();
+            var inrikes = JsonConvert.DeserializeObject<IEnumerable<Artikel>>(client.GetStringAsync("http://10.110.171.159/inrikes").Result).ToList();
+            var kultur = JsonConvert.DeserializeObject<IEnumerable<Artikel>>(client.GetStringAsync("http://10.110.171.79/kultur").Result).ToList();
+            var sport = JsonConvert.DeserializeObject<IEnumerable<Artikel>>(client.GetStringAsync("http://10.110.171.112:8080/sport").Result).ToList();
+            inrikes.AddRange(sport);
+            inrikes.AddRange(kultur);
+            return inrikes;
+        }
+    }
+}
+
+public class Artikel {
+    public string Rubrik { get; set; }
+    public string Brodtext { get; set; }
+}
---
+++ net-clarenews/README.md
@@ -0,0 +1,21 @@
+# Clarenews .NET Core API aggregator
+Aggregates the result from other Clarenews APIs
+
+### Prerequisites
+* .NET Core 2.0:
+* Clarenews Inrikes API
+* Clarenews Sport API
+* Clarenews Kultur API
+
+### Clone the repo and install dependencies
+```
+$ git clone git@github.com:ClaremontAB/devforum-microservices.git
+$ cd devforum-microservices/net-clarenews/Clarenews
+$ dotnet build
+```
+
+### Start the server
+```
+$ dotnet run
+```
+After server has been started, visit `localhost` in your web browser.
